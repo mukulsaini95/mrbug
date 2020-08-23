99e5460c46e5ca608ee20e4b1091116c25e6b557
mukul saini <mukul.saini@83incs.com>
2020-08-23 15:05:52 +0530

    webpack changes

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -65,36 +65,32 @@ module.exports = options => ({
         ],
       },
       {
-        test: /\.(jpg|png|gif)$/,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // Inline files smaller than 10 kB
-              limit: 10 * 1024,
-            },
-          },
+          'file-loader',
           {
             loader: 'image-webpack-loader',
-            options: process.env.NODE_ENV === "production" ? {
+            options: {
               mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
                 enabled: false,
-                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
-                // Try enabling it in your environment by switching the config to:
-                // enabled: true,
-                // progressive: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
               },
               gifsicle: {
                 interlaced: false,
               },
-              optipng: {
-                optimizationLevel: 7,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-            } : {},
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
           },
         ],
       },
